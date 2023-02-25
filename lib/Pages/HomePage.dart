b0dbcc4ae44790e66d2de6514c49a5b6926f6d82
Akshay <akshay0127.be21@chitkara.edu.in>
2023-02-25 21:43:10 +0530

    chapter4 completed

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import '../Drawer.dart';
 import '../changeNameCard.dart';
 
@@ -11,38 +12,55 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  TextEditingController _newControler = TextEditingController();
-  var myText = "Change Me";
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(Uri.parse(url));
+    data = jsonDecode(res.body);
+    print(data);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("NewApp"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-              child: ChangeNameCard(myText: myText, newControler: _newControler)),
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text("NewApp"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _newControler.text;
-          setState(() {
-            
-          });
-        },
-        child: Icon(Icons.refresh),
-      ),
-      drawer: MyDrawer()
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: data != null
+              ? ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        title: Text(data[index]["title"]),
+                        subtitle: Text("ID:${data[index]["id"]}"),
+                        leading: Image.network(data[index]["url"]),
+                      ),
+                    );
+                  },
+                  itemCount: data.length,
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {});
+          },
+          child: Icon(Icons.refresh),
+        ),
+        drawer: MyDrawer());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:newapp/Drawer.dart';
 import 'package:newapp/Pages/HomePage.dart';
 import 'package:newapp/bgimage.dart';
 import 'package:newapp/changeNameCard.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
