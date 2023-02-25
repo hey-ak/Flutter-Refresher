3a1a8341213a41da7c7abbb185205ede0f753cca
Akshay <akshay0127.be21@chitkara.edu.in>
2023-02-25 11:01:54 +0530

    statefull widget added

--- lib/main.dart
+++ lib/main.dart
@@ -8,30 +8,89 @@ void main() {
   ));
 }
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  TextEditingController _newControler = TextEditingController();
+  var myText = "Change Me";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("NewApp"),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+              child: Column(
+            children: [
+              Image.asset(
+                "assets/login.jpg",
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                myText,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: _newControler,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter Name Here",
+                    labelText: "Name",
+                  ),
+                ),
+              )
+            ],
+          )),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.edit),
+        onPressed: () {
+          myText = _newControler.text;
+          setState(() {
+            
+          });
+        },
+        child: Icon(Icons.refresh),
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-          
-              UserAccountsDrawerHeader(
-                accountName: Text("Akshay"),
-                accountEmail: Text("akshay0127.be21@chitkara.edu.in"),
-              currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage("https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8TWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"),),),
-            
+            UserAccountsDrawerHeader(
+              accountName: Text("Akshay"),
+              accountEmail: Text("akshay0127.be21@chitkara.edu.in"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8TWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"),
+              ),
+            ),
+
             ListTile(
               leading: Icon(Icons.person),
               title: Text("Account"),
